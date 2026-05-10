e446eecc163c140b951ca29d335eddda58dd2fb2
MadsFalk1 <161165032+MadsFalk1@users.noreply.github.com>
2026-05-10 23:46:59 +0200

    Empty repository

--- docs/javascripts/mermaid-init.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  if (window.mermaid) {
-    window.mermaid.initialize({
-      startOnLoad: true,
-      theme: document.body.getAttribute("data-md-color-scheme") === "slate" ? "dark" : "default"
-    });
-  }
-});
